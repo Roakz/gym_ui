26195c1af0fac8be67f7b92ca61bc4e129a22813
rory <rory_bell_88@hotmail.com>
2020-11-01 16:36:57 +1000

    indexTable component now accepts table header props as defined by the interface.

--- src/components/IndexTable.tsx
+++ src/components/IndexTable.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import Table from 'react-bootstrap/Table';
 
-const IndexTable: React.FC = () => {
+interface IndexTableProps {
+  columnHeaders?: Array<String>
+}
+
+function IndexTable(props: IndexTableProps){  
   return (
     <Table striped bordered hover>
   <thead>
     <tr>
       <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
+      {props.columnHeaders?.map(function(element, index){
+        return <th key={index}>{element}</th>
+      })}
     </tr>
   </thead>
   <tbody>
@@ -18,13 +22,16 @@ const IndexTable: React.FC = () => {
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
+      <td>yup</td>
     </tr>
     <tr>
       <td>2</td>
       <td>Jacob</td>
       <td>Thornton</td>
       <td>@fat</td>
+      <td>yup</td>
     </tr>
+    
   </tbody>
 </Table>
   )
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -4,13 +4,16 @@ import IndexTable from '../components/IndexTable'
 import SearchCard from '../components/SearchCard'
 
 const UsersPage: React.FC = () => {
+
+
   return (
     <div className="index-wrapper">
         <SearchCard />
+        <hr></hr>
         <p className="table-heading">
           Users
         </p>
-        <IndexTable />
+        <IndexTable columnHeaders={["First Name", "Last Name", "Username", "Reset Link" ]}/>
     </div>
   )
 };
